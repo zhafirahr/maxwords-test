1bc9f97bfaa65bc1a63a2fa139bcf19698f3b464
Zhafirah Prasetija <zprasetija@gmail.com>
2024-09-17 15:42:49 +0700

    first commit

---
+++ san_easy_max-word-of-sentences.js
@@ -0,0 +1,24 @@
+/**
+ * Direction:
+ * Find maximum number of words from given sentences.
+ *
+ * Expected:
+ * 23
+ */
+const sentences = [
+  'Mauris ultricies sed sapien eget malesuada. Suspendisse et aliquet odio, id ultrices erat. Praesent vehicula erat nulla. Aliquam a lorem urna. Donec.',
+  'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
+  'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
+];
+function result(sentences) {
+  // your code here
+  let maxWords = 0;
+  sentences.forEach(sentence => {
+    const wordCount = sentence.split('').length;
+    if(wordCount > maxWords) {
+        maxWords = wordCount;
+    }
+  });
+  return maxWords;
+}
+console.log(result(sentences));
